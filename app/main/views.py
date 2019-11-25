6f5903fa8a87d2c5550da9fb61e6c540ae8c25ea
AudreyCherrie <mitchelaudrey@gmail.com>
2019-11-25 15:37:29 +0300

    update my files

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,26 @@
 from flask_login import login_required
+from flask import render_template,rfrom flask import render_template,redirect,url_for, flash,request
+from flask_login import login_user
+from ..models import User
+from .forms import LoginForm,RegistrationFormedirect,url_for, flash,request
+from flask_login import login_user
+from ..models import User
+from .forms import LoginForm,RegistrationForm
 
 @main.route('/pitch/review/new/<int:id>', methods = ['GET','POST'])
 @login_required
 def new_review(id):
+
+@auth.route('/login',methods=['GET','POST'])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user,login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+
+        flash('Invalid username or Password')
+
+    title = "pitch login"
+    return render_template('auth/login.html',login_form = login_form,title=title)    
